196c5888493261d19ad5350c2538ba0f355e57ce
Alessandro Lima <alessandroslima@hotmail.com>
2023-09-11 20:45:38 -0700

    Solution for Question 3

---
+++ q3.sql
@@ -0,0 +1,9 @@
+SELECT E.EmployeeID, E.LastName, E.FirstName, ROUND(SUM(OD.Quantity * P.Price), 2) AS TotalSales
+FROM Employees E
+INNER JOIN Orders O ON E.EmployeeID = O.EmployeeID
+INNER JOIN OrderDetails OD ON O.OrderID = OD.OrderID
+INNER JOIN Products P ON OD.ProductID = P.ProductID
+WHERE O.OrderDate BETWEEN "1996-01-01" AND "1996-12-31"
+GROUP BY E.EmployeeID, E.LastName, E.FirstName
+ORDER BY TotalSales DESC
+LIMIT 1, 4
